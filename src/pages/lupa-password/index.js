5b1bd063e66e6aa046804b05ba2d8c3f60e98f3f
Danang Prakoso <prakosd@yahoo.co.id>
2021-01-10 19:54:35 +0700

    create pages

---
+++ src/pages/lupa-password/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ForgetPassword() {
+    return <h1>Halaman Lupa Password</h1>;
+}
+
+export default ForgetPassword;
---
+++ src/pages/registrasi/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Registration() {
+    return <h1>Halaman Registrasi</h1>;
+}
+
+export default Registration;
